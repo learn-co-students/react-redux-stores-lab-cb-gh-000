1e1bccb0233b264b7b9051828f76ebd6302563e7
tao liu <werewaswas@live.com>
2017-05-26 00:56:57 +0000

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,25 @@
 export default function createStore(reducer){
 // your code here!
+  let state
+  state = reducer(state, {});
+  let listeners = [];
 
+  function getState() {
+    return state
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    listeners.forEach( listener => listener());
+  }
+
+  function subscribe(listener) {
+    listeners.push(listener);
+  }
+
+  return {
+    getState: getState,
+    dispatch: dispatch,
+    subscribe: subscribe
+  }
 }
