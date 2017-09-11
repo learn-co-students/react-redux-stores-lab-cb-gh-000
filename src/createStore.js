e30acc6a5f8a60b8163521aa624e28c2a4b9c16d
Dominik Kurbiel <github email address>
2017-09-11 16:08:55 +0200

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,29 @@
+import countReducer from './reducers/countReducer';
+import candyReducer from './reducers/candyReducer';
+
 export default function createStore(reducer){
-// your code here!
+	
+	let state, listeners = [];
+	
+	const dispatch = (action) => {
+		state = reducer(state,action);
+		listeners.forEach(listener => listener())
+	}
+	
+	const subscribe = (listener) => {
+		listeners.push(listener);
+	}
+	
+	const getState = () => {
+		return state || 0;
+	}
+	
+	return {
+		getState,
+		dispatch,
+		subscribe
+	}
 
 }
+
+export const store = createStore(candyReducer);
